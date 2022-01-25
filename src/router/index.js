38286df90335df13389658b30d894b855219ee10
shang <shang.hsu@cloudnifier.com>
2022-01-25 17:02:36 +0800

    createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import ShowBox from '../views/ShowBox.vue'
 import NewBox from '../views/NewBox.vue'
@@ -25,7 +25,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history:createWebHistory(),
+  history:createWebHashHistory(),
   routes
 })
 
